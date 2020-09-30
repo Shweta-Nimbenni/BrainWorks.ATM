74ea372559a512c901b77ad1fcb465c517f5dd39
s.nimbenni1 <s.nimbenni1@gmail.com>
2020-09-30 16:23:07 +0530

    Add project files.

---
+++ BrainWorks.ATM.Data/ATMContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrainWorks.ATM.Data
+{
+    public class ATMContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    
+    {
+            string connectionString = "Data Source =SATYA\\SQLEXPRESS ; Initial Catalog = ATMDB; Integrated Security = True";
+            optionsBuilder.UseSqlServer(connectionString);
+    }
+}
+}
